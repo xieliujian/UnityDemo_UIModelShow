681905cf850f50f1397560b2696067e7a6f9a465
谢刘建 <377624289@qq.com>
2017-11-30 02:28:49 -0600

    UnityDemo_UIModelShow

---
+++ Assets/Scripts/ModelRot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRot : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * 5);
+	}
+}
---
+++ Assets/Scripts/UIModelShow.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIModelShow : MonoBehaviour
+{
+    public Transform mRole;
+
+    public Animator mAnimator;
+
+    public UIWidget mTouch;
+
+    public UISprite mStunBtn;
+
+    public UISprite mDeadBtn;
+
+    public UISprite mIdleBtn;
+
+    #region 内置函数
+
+    // Use this for initialization
+    void Start ()
+    {
+        UIEventListener.Get(mTouch.gameObject).onDrag = OnTouchDrag;
+        UIEventListener.Get(mStunBtn.gameObject).onClick = OnStunBtnClick;
+        UIEventListener.Get(mDeadBtn.gameObject).onClick = OnDeadBtnClick;
+        UIEventListener.Get(mIdleBtn.gameObject).onClick = OnIdleBtnClick;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #endregion
+
+    #region 回调函数
+
+    private void OnTouchDrag(GameObject go, Vector2 delta)
+    {
+        float yrot = mRole.localEulerAngles.y - (delta.x * 0.5f);
+        mRole.localEulerAngles = new Vector3(mRole.localEulerAngles.x , yrot, mRole.localEulerAngles.z);
+    }
+
+    private void OnStunBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("stun", true);
+    }
+
+    private void OnDeadBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("dead", true);
+    }
+
+    private void OnIdleBtnClick(GameObject go)
+    {
+        mAnimator.SetBool("stun", false);
+        mAnimator.SetBool("dead", false);
+    }
+
+    #endregion
+}
